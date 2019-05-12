f1e7c83ce9335909e143b1b4cbed56f20cdc1e09
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-13 00:10:31 +0300

    turn multers buffer into a readable string and send it to cloudinary. get a link back

---
+++ src/middlewares/uploadImg.js
@@ -0,0 +1,23 @@
+const { uploader } = require('../config/cloudinaryConfig');
+const { dataUri } = require('./multerUploads');
+
+
+exports.uploadImg = (req, res) => {
+  if (req.file) {
+    const file = dataUri(req).content;
+    return uploader.upload(file).then((result) => {
+      const image = result.url;
+      return res.status(200).json({
+        messge: 'Your image has been uploded successfully to cloudinary',
+        data: {
+          image,
+        },
+      });
+    }).catch(err => res.status(400).json({
+      messge: 'someting went wrong while processing your request',
+      data: {
+        err,
+      },
+    }));
+  }
+};
