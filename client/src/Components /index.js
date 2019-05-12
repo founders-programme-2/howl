4d54093ba3f1ddf3be8aa673c397b26894a82d3a
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-12 13:14:38 +0300

    add index.js. Import and export all components from this file to make code modular

---
+++ client/src/Components /index.js
@@ -0,0 +1,27 @@
+import Main from './Main';
+import About from './About';
+import Resources from './Resources';
+import WLM from './WLM';
+import AddStory from './AddStory';
+import Archive from './Archive';
+import Footer from './Footer';
+import Header from './Header';
+import Help from './Help';
+import Results from './Results';
+import Story from './Story';
+import Timeline from './Timeline';
+
+export {
+  Main,
+  About,
+  Resources,
+  WLM,
+  AddStory,
+  Archive,
+  Footer,
+  Header,
+  Help,
+  Results,
+  Story,
+  Timeline,
+};
