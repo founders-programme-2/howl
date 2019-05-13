5c13270d77081a3510674b31a4d75324298a4387
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-13 15:06:23 -0500

    Move  footer and header into Common components folder since they are shared by multiple pages

---
+++ client/src/Components/Common/Header/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as style from './Header.style';
+
+const Header = () => {
+  return (
+    <header>
+      <img />
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Women's Liberation</li>
+          <li>Feminist Resources</li>
+          <li>The Archive</li>
+          <li>Interactive Timeline</li>
+        </ul>
+        <button>Add Your Story</button>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- client/src/Components/Main/index.js
+++ client/src/Components/Main/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from '../Common/Header';
 
 const Main = () => {
   return <h1>Hello Main Page</h1>;
