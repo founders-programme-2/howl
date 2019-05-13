850273d19eda0629d41cfc1e2e39b9b9e9c167ff
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-13 14:45:19 +0300

    moved cloudinary config and multer to uploadImg file to make code easier to follow

--- src/middlewares/uploadImg.js
+++ src/middlewares/uploadImg.js
@@ -1,7 +1,38 @@
-const { uploader } = require('../config/cloudinaryConfig');
-const { dataUri } = require('./multerUploads');
+require('env2')('./config.env');
+
+const { config, uploader } = require('cloudinary');
+const multer = require('multer');
+const Datauri = require('datauri');
+const path = require('path');
+
+// setup cloudinary configuration
+
+exports.cloudinaryConfig = (req, res, next) => {
+  config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+  next();
+};
 
 
+// set up multer
+
+// setup multer to save the image to memory
+const storage = multer.memoryStorage();
+
+// Turn image into a buffer
+exports.multerUploads = multer({ storage }).single('image');
+
+const dUri = new Datauri();
+
+// Turn buffer into a string blob
+const dataUri = req => dUri.format(path.extname(req.file.originalname)
+  .toString(), req.file.buffer);
+
+
+// send string blob to cloudinary
 exports.uploadImg = (req, res) => {
   if (req.file) {
     const file = dataUri(req).content;
