df8eacfa7b42981cc55c6a99a0c066e867ceec65
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-13 15:34:11 -0500

    Finish basic setup of skeleton header

---
+++ client/src/Components/Common/Header/Header.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NavList = styled.ul`
+    list-style: none;
+`
+
+export const NavLink = styled.a`
+  font-family: 'Raleway', sans-serif;
+  font-weight: 900;
+  font-size: 1.5rem;
+  color: white;
+  list-style: none;
+`;
+
--- client/src/Components/Common/Header/index.js
+++ client/src/Components/Common/Header/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
-import * as style from './Header.style';
+import { NavLink, NavList } from './Header.style';
+import logoPath from '../../../Assets/logo.svg';
 
 const Header = () => {
   return (
     <header>
-      <img />
+      <img src={logoPath} alt="Howl logo" />
       <nav>
-        <ul>
-          <li>About</li>
-          <li>Women's Liberation</li>
-          <li>Feminist Resources</li>
-          <li>The Archive</li>
-          <li>Interactive Timeline</li>
-        </ul>
-        <button>Add Your Story</button>
+        <NavList>
+          <li>
+            <NavLink>About</NavLink>
+          </li>
+          <li>
+            <NavLink>Women's Liberation</NavLink>
+          </li>
+          <li>
+            <NavLink>Feminist Resources</NavLink>
+          </li>
+          <li>
+            <NavLink>The Archive</NavLink>
+          </li>
+          <li>
+            <NavLink>Interactive Timeline</NavLink>
+          </li>
+        </NavList>
+        <button type="button">Add Your Story</button>
       </nav>
     </header>
   );
