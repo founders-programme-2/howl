907ddfe85d33b328f89fc3dba673414c65fdc1ca
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-13 16:02:25 -0500

    Add sty;es for the nav, ul, and logo

--- client/src/Components/Common/Header/Header.style.js
+++ client/src/Components/Common/Header/Header.style.js
@@ -1,14 +1,31 @@
 import styled from 'styled-components';
 
+export const NavSection = styled.nav`
+  background-color: black;
+  display: flex;
+  flex-flow: row nowrap;
+  min-width: 100%;
+`;
+
 export const NavList = styled.ul`
-    list-style: none;
-`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  margin: auto;
+`;
+
+export const NavLi = styled.li`
+  text-align: center;
+`;
 
 export const NavLink = styled.a`
   font-family: 'Raleway', sans-serif;
   font-weight: 900;
-  font-size: 1.5rem;
+  font-size: 1.3rem;
   color: white;
   list-style: none;
 `;
 
+export const Logo = styled.img`
+  margin: 1rem;
+`;
--- client/src/Components/Common/Header/index.js
+++ client/src/Components/Common/Header/index.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { NavLink, NavList } from './Header.style';
+import { NavLink, NavList, NavSection, NavLi, Logo } from './Header.style';
 import logoPath from '../../../Assets/logo.svg';
 
 const Header = () => {
   return (
-    <header>
-      <img src={logoPath} alt="Howl logo" />
-      <nav>
-        <NavList>
-          <li>
-            <NavLink>About</NavLink>
-          </li>
-          <li>
-            <NavLink>Women's Liberation</NavLink>
-          </li>
-          <li>
-            <NavLink>Feminist Resources</NavLink>
-          </li>
-          <li>
-            <NavLink>The Archive</NavLink>
-          </li>
-          <li>
-            <NavLink>Interactive Timeline</NavLink>
-          </li>
-        </NavList>
-        <button type="button">Add Your Story</button>
-      </nav>
-    </header>
+    <NavSection>
+      <Logo src={logoPath} alt="Howl logo" />
+      <NavList>
+        <NavLi>
+          <NavLink>About Howl</NavLink>
+        </NavLi>
+        <NavLi>
+          <NavLink>Women&#39;s Liberation</NavLink>
+        </NavLi>
+        <NavLi>
+          <NavLink>Feminist Resources</NavLink>
+        </NavLi>
+        <NavLi>
+          <NavLink>The Archive</NavLink>
+        </NavLi>
+        <NavLi>
+          <NavLink>Interactive Timeline</NavLink>
+        </NavLi>
+      </NavList>
+      <button type="button">Add Your Story</button>
+    </NavSection>
   );
 };
 
