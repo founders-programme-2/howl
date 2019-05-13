de84ef244094c34eb6204b0ba3f3c6868239e63a
mhmdtshref <mhmdtshref@gmail.com>
2019-05-13 18:00:57 +0300

    Update app to use urlencoded and json modules #90

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = express();
 const router = require('./router');
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(router);
 
 app.get('*', (req, res) => {
