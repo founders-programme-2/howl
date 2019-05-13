1d0ce6bb01ec59218611ac2314d30f2bb63d83a5
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-13 14:25:22 -0500

    add basic fonts and sizes for headings and text

--- client/src/globalStyle.js
+++ client/src/globalStyle.js
@@ -1,5 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+export default createGlobalStyle`
 
-`
+    // import google fonts - Raleway (400/regular, 800, 900) and Roboto (regular)
+    @import url('https://fonts.googleapis.com/css?family=Raleway:400,800,900|Roboto');
+    
+    body {
+
+    }
+
+    // titles   
+    h1 {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 900;
+        font-size: 3rem;
+    }
+
+    // subtitle
+    h2 {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 800;
+        font-size: 2.5rem;
+    }
+
+    // nav, buttons etc
+    h3 {
+        font-family: 'Raleway', sans-serif;
+        font-weight: 900;
+        font-size: 1.5rem;
+    }
+
+    p {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.2rem;
+    }
+`;
