2c3dd57e426ed9f8766e2ece59bb4ba2c0b819bd
Kristina Jaggard <jaggardk@gmail.com>
2019-05-14 18:26:40 +0100

    create data file for resources

--- client/src/Components/Resources/index.js
+++ client/src/Components/Resources/index.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Resources = () => {
-  return <h1>Hello Resources page!</h1>;
+  return (
+    <Fragment>
+      <main>
+        <header>
+          <h1>Feminist Resources</h1>
+        </header>
+        <p>
+          While HOWL exists to fill a gap in the archival and sharing of
+          personal stories, many other thorough and valuable feminist resources
+          exist. If you are looking to learn more, please have a look at the
+          following:
+        </p>
+        <article>
+          <h2>The Feminist Library</h2>
+          <p>
+            The Feminist Library is a large collection of Women&#39;s Liberation
+            Movement literature based in London. They have been supporting
+            research, activist and community projects since 1975.
+          </p>
+        </article>
+        <article>
+          <h2>Timeline of the Women&#39;s Liberation Movement</h2>
+          <p>
+            Collected and held by the British Library, the Timeline of the
+            Women’s Liberation Movement details a number of significant
+            historical events.
+          </p>
+        </article>
+        <article>
+          <h2 />
+        </article>
+        To add: https://www.bl.uk/sisterhood/timeline
+        http://www.lse.ac.uk/Library/Collections/Collection-highlights/The-Womens-Library
+        http://feministlibrary.co.uk/ https://www.bishopsgate.org.uk/Library
+        https://southallblacksisters.org.uk/
+        https://finnmackay.wordpress.com/about/ https://womenslibrary.org.uk/
+        https://feministarchivenorth.org.uk/
+      </main>
+    </Fragment>
+  );
 };
 
 export default Resources;
