c774ce0c849f58a5e1c74e204d3dec436bf500e5
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-14 11:51:46 +0300

    moved menuProps styles to this file from AddStory.js

--- client/src/Components/AddStory/muiStyles.js
+++ client/src/Components/AddStory/muiStyles.js
@@ -30,3 +30,14 @@ exports.styles = theme => ({
     margin: theme.spacing.unit / 4,
   },
 });
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+exports.MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
