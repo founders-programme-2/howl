226fc68f7eb7d5237ac44f6f998ec3f0f792c63c
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-15 11:28:14 +0300

    wrap textfields in styled form component

--- client/src/Components/Add/ContactInfo/index.js
+++ client/src/Components/Add/ContactInfo/index.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { AddHead, AddSub, Para } from './ContactInfo.style';
+import { AddHead, AddSub, Para, Form } from './ContactInfo.style';
 import { withStyles, TextField } from '../../muIndex';
 import styles from './muiStyles';
 
@@ -12,28 +12,30 @@ const ContactInfo = ({ classes }) => {
         How might we get in touch with you if we have any questions about your
         input?
       </Para>
-      <TextField
-        id="name"
-        label="Name"
-        placeholder="Enter your name..."
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="email"
-        label="Email"
-        placeholder="Enter your email..."
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="phoneNum"
-        label="Phone Number"
-        placeholder="Enter your phone number..."
-        className={classes.textField}
-        helperText="e.g:(+44 20 7946 0009)"
-        margin="normal"
-      />
+      <Form>
+        <TextField
+          id="name"
+          label="Name"
+          placeholder="Enter your name..."
+          className={classes.textField}
+          margin="normal"
+        />
+        <TextField
+          id="email"
+          label="Email"
+          placeholder="Enter your email..."
+          className={classes.textField}
+          margin="normal"
+        />
+        <TextField
+          id="phoneNum"
+          label="Phone Number"
+          placeholder="Enter your phone number..."
+          className={classes.textField}
+          helperText="e.g:(+44 20 7946 0009)"
+          margin="normal"
+        />
+      </Form>
     </Fragment>
   );
 };
