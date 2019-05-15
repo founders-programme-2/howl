64af92960563fd247da27e38e2682d8e8e8f301e
mhmdtshref <mhmdtshref@gmail.com>
2019-05-15 12:45:32 +0300

    Creare initial footer component #82

---
+++ client/src/Components/Common/Footer/Footer.style.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const FooterDiv = styled.div`
+  min-width: 100%;
+  background-color: black;
+  display: felx;
+  flex-direction: row;
+  flex-wrap: nowrap;
+`;
+
+export const TitleDiv = styled.div`
+  width: 30%;
+  height: 100%;
+  background-color: blue;
+`;
+
+export const StyledTitle = styled.h2`
+  color: black;
+  text-align: center;
+  font-family: 'Raleway', sans-serif;
+`;
+
+export const FilterDiv = styled.div`
+  width: 40%;
+  height: 100%;
+  background-color: red;
+`;
+
+export const FormContainer = styled.div`
+  width: 100%;
+`;
