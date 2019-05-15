f0b1f58043796ce055ff564d989a51729e211d82
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-15 16:27:43 +0300

    fix indentation

--- client/src/Components/Form/Post/index.js
+++ client/src/Components/Form/Post/index.js
@@ -5,42 +5,42 @@ import { AttachImg, Input, Form, Label } from './Post.style';
 import styles from './muiStyles';
 
 const Post = ({ classes }) => {
-    return (
-        <Form>
-            <TextField
-                id="title"
-                label="Title"
-                placeholder="Enter your Title..."
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="details"
-                label="Details"
-                placeholder="Enter your story's details"
-                multiline
-                className={classes.textField}
-                margin="normal"
-            />
-            <TextField
-                id="imageCap"
-                label="Image Caption"
-                placeholder="Enter image caption..."
-                className={classes.textField}
-                margin="normal"
-            />
-            <Label htmlFor="inputImg">
-                <Input
-                    id="inputImg"
-                    label="upload file"
-                    type="file"
-                    accept="image/*"
-                    hidden
-                />
-                <AttachImg src={Attach} alt="attach" />
-            </Label>
-        </Form>
-    );
+  return (
+    <Form>
+      <TextField
+        id="title"
+        label="Title"
+        placeholder="Enter your Title..."
+        className={classes.textField}
+        margin="normal"
+      />
+      <TextField
+        id="details"
+        label="Details"
+        placeholder="Enter your story's details"
+        multiline
+        className={classes.textField}
+        margin="normal"
+      />
+      <TextField
+        id="imageCap"
+        label="Image Caption"
+        placeholder="Enter image caption..."
+        className={classes.textField}
+        margin="normal"
+      />
+      <Label htmlFor="inputImg">
+        <Input
+          id="inputImg"
+          label="upload file"
+          type="file"
+          accept="image/*"
+          hidden
+        />
+        <AttachImg src={Attach} alt="attach" />
+      </Label>
+    </Form>
+  );
 };
 
 export default withStyles(styles)(Post);
