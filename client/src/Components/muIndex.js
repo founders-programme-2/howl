412b4c5bf68505bf0f388ad87be8c22edf2c33a8
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-15 11:04:35 +0300

    import MUI components to build Details component

--- client/src/Components/muIndex.js
+++ client/src/Components/muIndex.js
@@ -1,4 +1,17 @@
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import FormGroup from '@material-ui/core/FormGroup';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
-export { withStyles, TextField };
+export {
+  withStyles,
+  TextField,
+  FormGroup,
+  Radio,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+};
