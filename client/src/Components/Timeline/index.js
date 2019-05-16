ae8a0aff2ed8ec1f3ed67ab2ed9eb850cee7dac6
Kristina Jaggard <jaggardk@gmail.com>
2019-05-16 18:50:55 +0100

    add basic content to archive and timeline pages

--- client/src/Components/Timeline/index.js
+++ client/src/Components/Timeline/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import navigationUrls from '../../constants/navigationUrls';
+
 
 const Timeline = () => {
+  const { ARCHIVE_URL } = navigationUrls;
   return (
     <main>
-      <h1>Hello Timeline page!</h1>
+      <header>
+        <h1>The HOWL Timeline</h1>
+      </header>
+      <p>
+        Browse our timeline by using the controls at the bottom of this page to 
+        filter your results by category, location, tags or year. Above the search controls, you'll find 
+        a yellow bar of dates. This allows you to view stories from between a time span of four to five 
+        years.
+        <br />
+        <br />
+        Prefer a simpler approach? Take a look at the <Link to={ARCHIVE_URL}>archive</Link> for a text-based experience.
+      </p>
     </main>
   );
 };
