026d473dd2230c8b762ffef46aac5964ef51b074
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-16 15:37:37 -0500

    Add styles for centered sections, images, highlights, and final subtitle

--- client/src/Components/Help/Help.style.js
+++ client/src/Components/Help/Help.style.js
@@ -1,5 +1,24 @@
 import styled from 'styled-components';
 
-export default styled.h2`
+export const H2Sml = styled.h2`
   font-size: 1.2rem;
+`;
+
+export const ImgExample = styled.img`
+  margin: 1rem;
+`;
+
+export const SectionCntr = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const Highlight = styled.span`
+  color: var(--highlight-color);
+  font-weight: 800;
+`;
+
+export const CallToAction = styled.h2`
+  text-align: center;
 `;
