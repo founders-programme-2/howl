5ed1387b69ed015c37ff515560cc87b7ea23565c
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-16 13:19:08 +0300

    fixed eslint errors

--- client/src/Components/About/About.style.js
+++ client/src/Components/About/About.style.js
@@ -1,32 +1,32 @@
 import styled from 'styled-components';
 
 export const Sheader = styled.header`
-    margin: 4rem 2rem 2rem;
-    padding-left: 3rem;
+  margin: 4rem 2rem 2rem;
+  padding-left: 3rem;
 `;
 
 export const Sh1 = styled.h1`
-    letter-spacing: 0.2rem;  
-    font-size: 4rem;
-    text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  font-size: 4rem;
+  text-transform: uppercase;
 `;
 
 export const Sh2 = styled.h2`
-    font-size: 2rem;
-    margin: 2rem 2rem 0;
-    padding-left: 3rem;
+  font-size: 2rem;
+  margin: 2rem 2rem 0;
+  padding-left: 3rem;
 `;
 
 export const Sp = styled.p`
-    margin: 0rem 2rem;
-    padding-left: 3rem;
-    font-variant: small-caps;
-    line-height: 1.5rem;
-    text-transform: lowercase;
-    letter-spacing: 0.1rem;
-    font-size: 1.2rem;
+  margin: 0rem 2rem;
+  padding-left: 3rem;
+  font-variant: small-caps;
+  line-height: 1.5rem;
+  text-transform: lowercase;
+  letter-spacing: 0.1rem;
+  font-size: 1.2rem;
 `;
 
 export const Smain = styled.main`
-    margin-bottom: 6rem;
+  margin-bottom: 6rem;
 `;
