db7a8cdc58d3aa0113f5ba36af849e1a9b3f349d
Kristina Jaggard <jaggardk@gmail.com>
2019-05-16 11:26:06 +0100

    style wlm page

---
+++ client/src/Components/Wlm/Wlm.style.js
@@ -0,0 +1,16 @@
+
+import styled from 'styled-components';
+
+export const Sp = styled.p`
+margin: 0rem 2rem;
+padding-left: 3rem;
+font-variant: small-caps;
+line-height: 1.5rem;
+text-transform: lowercase;
+letter-spacing: 0.1rem;
+font-size: 1.2rem;
+`;
+
+export const Smain = styled.main`
+    margin-bottom: 6rem;
+`;
--- client/src/Components/Wlm/index.js
+++ client/src/Components/Wlm/index.js
@@ -1,25 +1,31 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import { Sp, Smain } from './Wlm.style';
 
 const Wlm = () => {
   return (
-    <Fragment>
-      <main>
+      <Smain>
         <header>
-          <h1>A History of the Women&#39;s Liberation Movement</h1>
+          <h1>Women&#39;s Liberation Movement</h1>
         </header>
-        <p>
-          The Women’s Liberations Movement (WLM) in the UK arose out of the rich
+        <Sp>
+          The Women’s Liberation Movement (WLM) in the UK arose out of the rich
           mix of 1960s social upheavals and events around the world. Here in the
           UK, trade union women were putting equal-pay back on the agenda,
           Vietnam War protests were pulling 1000s of people out onto the
           streets, the Gay Liberation Front formed and Black Power
           demonstrations took on racism. Women were engaged in all these areas -
-          and more. In the US women had already started to band together, making
+          and more. 
+          <br/>
+          <br/>
+          In the US women had already started to band together, making
           demands, creating trouble - and sending messages across oceans where
           they were eagerly received. We took all these influences and
           experiences and started an incendiary and creative conversation among
           ourselves which quickly flamed into the UK Women’s Liberation
-          Movement. Women gathered in small groups (later consciousness-raising
+          Movement. 
+          <br/>
+          <br/>
+          Women gathered in small groups (later consciousness-raising
           groups) to share experiences collectively. At the same time the need
           to act, to challenge, to change, led to demands, campaigns and
           projects. All through the 1970s and 80s the movement’s vitality
@@ -28,12 +34,14 @@ const Wlm = () => {
           everything to do with women’s roles, activism, the patriarchy, and our
           aims as a movement. The differences and divisions between women also
           led to difficult and sometimes painful struggles. Who said liberation
-          was ever ‘plain sailing’!? After some years of relative quiescence in
+          was ever ‘plain sailing’!? 
+          <br/>
+          <br/>
+          After some years of relative quiescence in
           the 1990s, new ‘waves’ of feminists have made themselves known. Much
           has been accomplished - much more is needed.
-        </p>
-      </main>
-    </Fragment>
+        </Sp>
+      </Smain>
   );
 };
 
