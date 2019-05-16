84e4fe561001538879ba745d64bdd99247fcf617
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-16 23:19:19 +0300

    implement conditional rendering based on what kind of post is selected

--- client/src/Components/Form/Post/index.js
+++ client/src/Components/Form/Post/index.js
@@ -1,44 +1,116 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Attach from './attach.png';
 import { TextField, withStyles } from '../../muIndex';
 import { AttachImg, Input, Form, Label } from './Post.style';
 import styles from './muiStyles';
 
-const Post = ({ classes }) => {
+const Post = ({ classes, title, details, imageCap, textChange, radio }) => {
   return (
     <Form>
-      <TextField
-        id="title"
-        label="Title"
-        placeholder="Enter your Title..."
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="details"
-        label="Details"
-        placeholder="Enter your story's details"
-        multiline
-        className={classes.textField}
-        margin="normal"
-      />
-      <TextField
-        id="imageCap"
-        label="Image Caption"
-        placeholder="Enter image caption..."
-        className={classes.textField}
-        margin="normal"
-      />
-      <Label htmlFor="inputImg">
-        <Input
-          id="inputImg"
-          label="upload file"
-          type="file"
-          accept="image/*"
-          hidden
-        />
-        <AttachImg src={Attach} alt="attach" />
-      </Label>
+      {radio === 'textPost' ? (
+        <Fragment>
+          <TextField
+            id="title"
+            label="Title"
+            name="title"
+            value={title}
+            placeholder="Enter your Title..."
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('title')}
+          />
+          <TextField
+            id="details"
+            label="Details"
+            name="details"
+            value={details}
+            placeholder="Enter your story's details"
+            multiline
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('details')}
+          />
+        </Fragment>
+      ) : null}
+      {radio === 'imagePost' ? (
+        <Fragment>
+          <TextField
+            id="title"
+            label="Title"
+            name="title"
+            value={title}
+            placeholder="Enter your Title..."
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('title')}
+          />
+          <TextField
+            id="imageCap"
+            label="Image Caption"
+            name="imageCap"
+            value={imageCap}
+            placeholder="Enter image caption..."
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('imageCap')}
+          />
+          <Label htmlFor="inputImg">
+            <Input
+              id="inputImg"
+              label="upload file"
+              type="file"
+              accept="image/*"
+              hidden
+            />
+            <AttachImg src={Attach} alt="attach" />
+          </Label>
+        </Fragment>
+      ) : null}
+      {radio === 'both' ? (
+        <Fragment>
+          <TextField
+            id="title"
+            label="Title"
+            name="title"
+            value={title}
+            placeholder="Enter your Title..."
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('title')}
+          />
+          <TextField
+            id="details"
+            label="Details"
+            name="details"
+            value={details}
+            placeholder="Enter your story's details"
+            multiline
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('details')}
+          />
+          <TextField
+            id="imageCap"
+            label="Image Caption"
+            name="imageCap"
+            value={imageCap}
+            placeholder="Enter image caption..."
+            className={classes.textField}
+            margin="normal"
+            onChange={textChange('imageCap')}
+          />
+          <Label htmlFor="inputImg">
+            <Input
+              id="inputImg"
+              label="upload file"
+              type="file"
+              accept="image/*"
+              hidden
+            />
+            <AttachImg src={Attach} alt="attach" />
+          </Label>
+        </Fragment>
+      ) : null}
     </Form>
   );
 };
