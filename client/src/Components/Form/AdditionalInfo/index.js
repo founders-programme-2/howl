5fbb720c937477f9dd55899aee3a255f48e15462
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-16 12:03:17 +0300

    make AdditionalInfo component and add basic mui styles

---
+++ client/src/Components/Form/AdditionalInfo/index.js
@@ -0,0 +1,83 @@
+import React, { Fragment } from 'react';
+import {
+  withStyles,
+  FormControl,
+  Input,
+  InputLabel,
+  MenuItem,
+  ListItemText,
+  Select,
+  Checkbox,
+  Chip,
+  FormControlLabel,
+} from '../../muIndex';
+import { styles, MenuProps } from './muiStyles';
+import { arrTags, arrCategory } from '../data.json';
+
+const AdditionalInfo = ({
+  classes,
+  tags,
+  tagsChange,
+  category,
+  categoryChange,
+  infoTrue,
+  checkboxChange,
+}) => {
+  return (
+    <Fragment>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="selectMultipleCheckbox">Tags</InputLabel>
+        <Select
+          aria-label="tags"
+          multiple
+          value={tags}
+          onChange={tagsChange}
+          input={<Input id="selectMultipleCheckbox" />}
+          renderValue={selected => (
+            <div className={classes.chips}>
+              {selected.map(value => (
+                <Chip key={value} label={value} className={classes.chip} />
+              ))}
+            </div>
+          )}
+          MenuProps={MenuProps}
+        >
+          {arrTags.map(tag => (
+            <MenuItem key={tag} value={tag}>
+              <Checkbox checked={tags.indexOf(tag) > -1} />
+              <ListItemText primary={tag} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="category">Category</InputLabel>
+        <Select
+          aria-label="category"
+          value={category}
+          onChange={categoryChange}
+          inputProps={{
+            name: 'category',
+            id: 'category',
+          }}
+        >
+          {arrCategory.map(cat => (
+            <MenuItem value={cat}>{cat}</MenuItem>
+          ))}
+        </Select>
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={infoTrue}
+              onChange={checkboxChange}
+              value="infoTrue"
+            />
+          }
+          label="I certify that this information is true to the best of my knowledge and contains no personal attacks."
+        />
+      </FormControl>
+    </Fragment>
+  );
+};
+
+export default withStyles(styles)(AdditionalInfo);
