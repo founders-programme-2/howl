683ad5cfba02716752a04dd9ad46815dded278b9
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-16 15:37:03 -0500

    Add example pictures, center them, and center final subtitle

--- client/src/Components/Help/index.js
+++ client/src/Components/Help/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import H2Sml from './Help.style';
+import {
+  H2Sml,
+  ImgExample,
+  SectionCntr,
+  Highlight,
+  CallToAction,
+} from './Help.style';
+import timeline from '../../assets/helpTimeline.svg';
+import dropdowns from '../../assets/helpDropdowns.svg';
+import searchInput from '../../assets/helpSearchInput.svg';
+import searchBtn from '../../assets/helpSearchBtn.svg';
 
 const Help = () => {
   return (
@@ -8,54 +18,64 @@ const Help = () => {
         <h1>How to Search</h1>
       </header>
 
-      <section>
+      <SectionCntr>
         <H2Sml>Where to Start</H2Sml>
         <p>
-          You have the option to search our archive by section of time,
-          category, location, tags, year, and your own search queries. These
-          options can be viewed one at a time, or in conjunction with others.
-          Please be sure to make all of your selections before pressing "Search"
+          You have the option to search our archive by{' '}
+          <Highlight>
+            section of time, category, location, tags, year, and your own search
+            queries
+          </Highlight>
+          . These options can be viewed one at a time, or in conjunction with
+          others. Please be sure to make all of your selections before pressing
+          <Highlight>"Search"</Highlight>
         </p>
-      </section>
+      </SectionCntr>
 
-      <section>
+      <SectionCntr>
         <H2Sml>Searching by Section of Time</H2Sml>
         <p>
           Clicking on any of the sections of time on the timeline will return
           only the results for that period (e.g. 1960 - 1965). Select one and
-          then press "Search", or continue to select other parameters first.
+          then press <Highlight>"Search"</Highlight>, or continue to select
+          other parameters first.
         </p>
-        <img src="" alt="" />
-      </section>
+        <ImgExample src={timeline} alt="Example of clickable timeline" />
+      </SectionCntr>
 
-      <section>
+      <SectionCntr>
         <H2Sml>Searching by Category, Location, Tags, or Year</H2Sml>
         <p>
           Clicking on the small arrow on the edge any of these fields will give
           you a list of options to choose from for filtering your search
           results. Click on one to select it. Select one and then press
-          "Search", or continue to select other parameters first.
+          <Highlight>"Search"</Highlight>, or continue to select other
+          parameters first.
         </p>
-        <img src="" alt="" />
-      </section>
+        <ImgExample src={dropdowns} alt="Dropdown selector example" />
+      </SectionCntr>
 
-      <section>
+      <SectionCntr>
         <H2Sml>Searching with your own search Queries</H2Sml>
         <p>
           By clicking on the empty input field with the text “search ...” you
           will be able to type in your own search queries. DO so, and then press
-          "Search", or continue to select other parameters first.
+          <Highlight>"Search"</Highlight>, or continue to select other
+          parameters first.
         </p>
-        <img src="" alt="" />
-      </section>
+        <ImgExample src={searchInput} alt="Search input example" />
+      </SectionCntr>
 
-      <section>
+      <SectionCntr>
         <H2Sml>Submitting your search</H2Sml>
-        <p>Once you have made all of your selections, simply click “Search”!</p>
-        <img src="" alt="" />
-      </section>
+        <p>
+          Once you have made all of your selections, simply click{' '}
+          <Highlight>"Search"</Highlight>!
+        </p>
+        <ImgExample src={searchBtn} alt="Search button example" />
+      </SectionCntr>
 
-      <h2>Now give it a try yourself!</h2>
+      <CallToAction>Now give it a try yourself!</CallToAction>
     </main>
   );
 };
