094804b06e50641c39b60454a970223949060566
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2019-05-16 15:45:04 -0500

    Replace quotation marks with '&quot;' to make the linter happy

--- client/src/Components/Help/index.js
+++ client/src/Components/Help/index.js
@@ -28,7 +28,7 @@ const Help = () => {
           </Highlight>
           . These options can be viewed one at a time, or in conjunction with
           others. Please be sure to make all of your selections before pressing
-          <Highlight>"Search"</Highlight>
+          <Highlight>&quot;Search&quot;</Highlight>
         </p>
       </SectionCntr>
 
@@ -37,7 +37,7 @@ const Help = () => {
         <p>
           Clicking on any of the sections of time on the timeline will return
           only the results for that period (e.g. 1960 - 1965). Select one and
-          then press <Highlight>"Search"</Highlight>, or continue to select
+          then press <Highlight>&quot;Search&quot;</Highlight>, or continue to select
           other parameters first.
         </p>
         <ImgExample src={timeline} alt="Example of clickable timeline" />
@@ -50,7 +50,7 @@ const Help = () => {
           Clicking on the small arrow on the edge any of these fields will give
           you a list of options to choose from for filtering your search
           results. Click on one to select it. Select one and then press
-          <Highlight>"Search"</Highlight>, or continue to select other
+          <Highlight>&quot;Search&quot;</Highlight>, or continue to select other
           parameters first.
         </p>
         <ImgExample src={dropdowns} alt="Dropdown selector example" />
@@ -62,7 +62,7 @@ const Help = () => {
         <p>
           By clicking on the empty input field with the text “search ...” you
           will be able to type in your own search queries. DO so, and then press
-          <Highlight>"Search"</Highlight>, or continue to select other
+          <Highlight>&quot;Search&quot;</Highlight>, or continue to select other
           parameters first.
         </p>
         <ImgExample src={searchInput} alt="Search input example" />
@@ -73,7 +73,7 @@ const Help = () => {
         <H2Sml>Submitting your search</H2Sml>
         <p>
           Once you have made all of your selections, simply click{' '}
-          <Highlight>"Search"</Highlight>!
+          <Highlight>&quot;Search&quot;</Highlight>!
         </p>
         <ImgExample src={searchBtn} alt="Search button example" />
         <figcaption>Example of the footer search button</figcaption>
