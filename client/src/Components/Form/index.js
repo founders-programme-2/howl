df0d9085be2b3d5323460c7b7000fb007803021b
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-16 23:13:49 +0300

    make state/handlers for all child components and pass them down as props

--- client/src/Components/Form/index.js
+++ client/src/Components/Form/index.js
@@ -6,10 +6,19 @@ import AdditionalInfo from './AdditionalInfo';
 
 class Form extends Component {
   state = {
-    radio: '',
+    radio: 'textPost',
     tags: [],
     category: '',
     infoTrue: false,
+    name: '',
+    email: '',
+    phone: '',
+    location: '',
+    selectedDate: new Date('January 1960'),
+    title: '',
+    details: '',
+    imageCap: '',
+    wlmConnection: '',
   };
 
   radioChange = event => {
@@ -17,24 +26,79 @@ class Form extends Component {
   };
 
   tagsChange = event => {
-    this.setState({ tags: event.target.value });
+    this.setState({ tags: event.target.value }, () => {
+      console.log(this.state.tags);
+    });
   };
 
   categoryChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      console.log(this.state[event.target.name]);
+    });
   };
 
-  checkboxChange = event => {
-    this.setState({ [event.target.value]: event.target.checked });
+  checkboxChange = name => event => {
+    this.setState({ [name]: event.target.checked }, () => {
+      console.log(this.state[name]);
+    });
+  };
+
+  textChange = name => event => {
+    this.setState({ [name]: event.target.value }, () => {
+      console.log(this.state[name]);
+    });
+  };
+
+  handleDateChange = date => {
+    const options = {
+      year: 'numeric',
+      month: 'long',
+    };
+    const dateModified = date.toLocaleDateString('en-US', options);
+    this.setState({ selectedDate: dateModified }, () => {
+      console.log(this.state.selectedDate);
+    });
   };
 
   render() {
-    const { radio, tags, category, infoTrue } = this.state;
+    const {
+      radio,
+      tags,
+      category,
+      infoTrue,
+      name,
+      email,
+      phone,
+      location,
+      selectedDate,
+      title,
+      details,
+      imageCap,
+      wlmConnection,
+    } = this.state;
     return (
       <main>
-        <ContactInfo />
-        <Details radio={radio} radioChange={this.radioChange} />
-        <Post />
+        <ContactInfo
+          name={name}
+          email={email}
+          phone={phone}
+          textChange={this.textChange}
+        />
+        <Details
+          radio={radio}
+          radioChange={this.radioChange}
+          location={location}
+          selectedDate={selectedDate}
+          textChange={this.textChange}
+          handleDateChange={this.handleDateChange}
+        />
+        <Post
+          title={title}
+          details={details}
+          imageCap={imageCap}
+          textChange={this.textChange}
+          radio={radio}
+        />
         <AdditionalInfo
           tags={tags}
           category={category}
@@ -42,6 +106,8 @@ class Form extends Component {
           tagsChange={this.tagsChange}
           categoryChange={this.categoryChange}
           checkboxChange={this.checkboxChange}
+          wlmConnection={wlmConnection}
+          textChange={this.textChange}
         />
       </main>
     );
