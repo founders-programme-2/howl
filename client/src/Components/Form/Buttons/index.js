aed61fff27179511b989be9bdb8356df7f96352a
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-18 16:55:08 +0300

    make Buttons component that will include the back and submit buttons

--- client/src/Components/Form/Buttons/index.js
+++ client/src/Components/Form/Buttons/index.js
@@ -5,13 +5,13 @@ import navigationUrls from '../../../constants/navigationUrls';
 
 const { ADD_URL } = navigationUrls;
 
-const Buttons = () => {
+const Buttons = ({ handleSubmit }) => {
   return (
     <ButtonsContainer>
       <Link to={ADD_URL}>
         <GoBack type="button">Go Back</GoBack>
       </Link>
-      <Submit>Submit</Submit>
+      <Submit onClick={handleSubmit}>Submit</Submit>
     </ButtonsContainer>
   );
 };
