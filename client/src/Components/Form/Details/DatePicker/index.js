0bf9da87104de40f861c3eb7cc441a55ed3ea9c1
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-20 10:06:05 +0300

    wrap datePicker in a container to style and add props/calsses for style

--- client/src/Components/Form/Details/DatePicker/index.js
+++ client/src/Components/Form/Details/DatePicker/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '../../../muIndex';
+import style from './muiStyles';
 
-const DateFun = ({ selectedDate, handleDateChange }) => {
+const DateFun = ({ selectedDate, handleDateChange, classes }) => {
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
-      <DatePicker
-        variant="inline"
-        openTo="year"
-        views={['year', 'month']}
-        label="Year and Month"
-        helperText="When did this happen?"
-        value={selectedDate}
-        onChange={handleDateChange}
-      />
+      <Grid className={classes.grid}>
+        <DatePicker
+          variant="dialog"
+          openTo="year"
+          views={['year', 'month']}
+          label="Year and Month"
+          helperText="When did this happen?"
+          value={selectedDate}
+          onChange={handleDateChange}
+          FormHelperTextProps={{
+            classes: {
+              root: classes.helperText,
+            },
+          }}
+        />
+      </Grid>
     </MuiPickersUtilsProvider>
   );
 };
 
-export default DateFun;
+export default withStyles(style)(DateFun);
