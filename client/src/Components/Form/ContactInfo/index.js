f8a286799dd44607c931d19e78845676b4919641
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-20 11:16:04 +0300

    remove form because of browser warnings

--- client/src/Components/Form/ContactInfo/index.js
+++ client/src/Components/Form/ContactInfo/index.js
@@ -12,62 +12,61 @@ const ContactInfo = ({ classes, name, email, phone, textChange }) => {
         content, we will get in touch using your contact details. We will not
         use your details to send you spam mail.
       </p>
-      <form>
-        <TextField
-          id="name"
-          name="name"
-          value={name}
-          label="Name"
-          placeholder="Enter your name..."
-          className={classes.textField}
-          margin="normal"
-          onChange={textChange('name')}
-          InputProps={{
-            disableUnderline: true,
-          }}
-          InputLabelProps={{
-            className: classes.label,
-          }}
-        />
-        <TextField
-          id="email"
-          name="email"
-          value={email}
-          label="Email"
-          placeholder="Enter your email..."
-          className={classes.textField}
-          margin="normal"
-          onChange={textChange('email')}
-          InputProps={{
-            disableUnderline: true,
-            classes: {
-              input: classes.input,
-            },
-          }}
-          InputLabelProps={{
-            className: classes.label,
-          }}
-        />
-        <TextField
-          id="phoneNum"
-          name="phone"
-          value={phone}
-          label="Phone Number"
-          placeholder="Enter your phone number. e.g:(+44 20 7946 0009)"
-          className={classes.textField}
-          margin="normal"
-          onChange={textChange('phone')}
-          InputProps={{
-            disableUnderline: true,
-            classes: {
-              input: classes.input,
-            },
-          }}
-          InputLabelProps={{
-            className: classes.label,
-          }}
-        />
-      </form>
+
+      <TextField
+        id="name"
+        name="name"
+        value={name}
+        label="Name"
+        placeholder="Enter your name..."
+        className={classes.textField}
+        margin="normal"
+        onChange={textChange('name')}
+        InputProps={{
+          disableUnderline: true,
+        }}
+        InputLabelProps={{
+          className: classes.label,
+        }}
+      />
+      <TextField
+        id="email"
+        name="email"
+        value={email}
+        label="Email"
+        placeholder="Enter your email..."
+        className={classes.textField}
+        margin="normal"
+        onChange={textChange('email')}
+        InputProps={{
+          disableUnderline: true,
+          classes: {
+            input: classes.input,
+          },
+        }}
+        InputLabelProps={{
+          className: classes.label,
+        }}
+      />
+      <TextField
+        id="phoneNum"
+        name="phone"
+        value={phone}
+        label="Phone Number"
+        placeholder="Enter your phone number. e.g:(+44 20 7946 0009)"
+        className={classes.textField}
+        margin="normal"
+        onChange={textChange('phone')}
+        InputProps={{
+          disableUnderline: true,
+          classes: {
+            input: classes.input,
+          },
+        }}
+        InputLabelProps={{
+          className: classes.label,
+        }}
+      />
     </Fragment>
   );
 };
