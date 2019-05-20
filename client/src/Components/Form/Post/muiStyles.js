8333b15959ea2374b48f4f83f225d301051902b0
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-20 10:09:52 +0300

    defined style rules for mui components inside Post component

--- client/src/Components/Form/Post/muiStyles.js
+++ client/src/Components/Form/Post/muiStyles.js
@@ -2,7 +2,28 @@ const styles = () => ({
   textField: {
     marginLeft: '5%',
     marginRight: '5%',
-    width: '200px',
+    width: '50%',
+    background: 'white',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: 4,
+    paddingLeft: '2%',
+  },
+  textFieldDetails: {
+    width: '90%',
+  },
+  label: {
+    paddingLeft: '2%',
+  },
+  formControlLabel: {
+    marginLeft: '2%',
+    display: 'block',
+    paddingLeft: '2%',
+    fontSize: 14,
+  },
+  uploadIcon: {
+    marginLeft: '2%',
+    display: 'block',
+    paddingLeft: '2%',
   },
 });
 
