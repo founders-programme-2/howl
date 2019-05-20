32803d2045fee76d670720d4780d128a10b07f39
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-20 10:06:39 +0300

    style mui components on DatePicker

---
+++ client/src/Components/Form/Details/DatePicker/muiStyles.js
@@ -0,0 +1,21 @@
+const styles = () => ({
+  helperText: {
+    fontFamily: 'Roboto',
+    fontVariant: 'none',
+    fontSize: 14,
+  },
+  grid: {
+    width: '25%',
+    padding: '0.5%',
+    display: 'flex',
+    justifyContent: 'center',
+    marginLeft: '5%',
+    marginRight: '5%',
+    background: 'white',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+    borderRadius: 4,
+    paddingLeft: '2%',
+  },
+});
+
+export default styles;
