08d4b287fdb885d909c3f1754e4ce99fc0cfb80e
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-20 20:37:37 +0300

    merged with staging - take 2

--- client/src/Components/Add/Add.style.js
+++ client/src/Components/Add/Add.style.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const NavBtn = styled.button`
+export const Sbtn = styled.button`
   text-transform: uppercase;
   transition: all 0.2s ease-in-out;
   font-size: 1.2rem;
@@ -12,9 +12,4 @@ export const NavBtn = styled.button`
     cursor: pointer;
     transform: scale(1.1);
   }
-`;
-
-export const BtnRight = styled.div`
-  display: flex;
-  flex-direction: row-reverse;
 `;
