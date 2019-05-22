e70493cf70fa5e6f3694840240edc79ea82fcaca
Kristina Jaggard <jaggardk@gmail.com>
2019-05-22 11:06:33 +0100

    route /archive/feed

--- src/router.js
+++ src/router.js
@@ -3,12 +3,10 @@ const controllers = require('./controllers');
 
 const { uploadImg, multerUploads, cloudinaryConfig } = require('./middlewares/uploadImg');
 
-
 const router = express.Router();
 
-
+router.get('/archive/feed', controllers.Feed.feed);
 router.post('/upload', [cloudinaryConfig, multerUploads, uploadImg]);
-
 router.post('/posts/create', controllers.Story.create);
 
 module.exports = router;
