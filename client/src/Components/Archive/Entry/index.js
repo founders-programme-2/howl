b50e996bebe0baf63d48ca3fc948ef92122f23e8
Kristina Jaggard <jaggardk@gmail.com>
2019-05-22 13:05:02 +0100

    shorten incoming body

--- client/src/Components/Archive/Entry/index.js
+++ client/src/Components/Archive/Entry/index.js
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react';
 
 const Entry = ({ title, details }) => {
+  const shortenDetails = bodyP => {
+    const splitDetails = bodyP.split(' ');
+    const newArr = splitDetails.slice(0, 30);
+    newArr.push('...');
+    const bodyString = newArr.join(' ');
+    return bodyString;
+  };
   return (
     <Fragment>
       <h3>{title}</h3>
-      <p>{details}</p>
+      <p>{shortenDetails(details)}</p>
     </Fragment>
   );
 };
