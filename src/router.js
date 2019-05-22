36852e11287cca32d16464d5283a6990c6e40e1b
Kristina Jaggard <jaggardk@gmail.com>
2019-05-22 19:41:08 +0100

    refactor to use one type of import in src/router

--- src/router.js
+++ src/router.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const { Story, getStory } = require('./controllers');
+const { Story, getStory, Feed } = require('./controllers');
 
 const { uploadImg, multerUploads, cloudinaryConfig } = require('./middlewares/uploadImg');
 
 const router = express.Router();
 
 router.get('/posts/:id', getStory.getStory);
-router.get('/archive/feed', controllers.Feed.feed);
+router.get('/archive/feed', Feed.feed);
 router.post('/upload', [cloudinaryConfig, multerUploads, uploadImg]);
 router.post('/posts/create', Story.create);
 
