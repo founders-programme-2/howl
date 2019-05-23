3ebed0a1d47e2f13ad0b59f554941c443bbadfae
mhmdtshref <mhmdtshref@gmail.com>
2019-05-23 14:33:48 +0300

    Add event to test using enzyme package #181

--- client/src/Components/Common/Footer/index.test.js
+++ client/src/Components/Common/Footer/index.test.js
@@ -1,9 +1,48 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme from 'enzyme';
 import { BrowserRouter } from 'react-router-dom';
 import Footer from './index';
 
-test('Natural', () => {
-  const footer = renderer.create(<BrowserRouter> <Footer /> </BrowserRouter>).toJSON();
-  expect(footer).toMatchSnapshot();
+Enzyme.configure({ adapter: new Adapter() });
+
+const { mount } = Enzyme;
+
+describe('Footer Component Test', () => {
+  const footer = mount(
+    <BrowserRouter>
+      <Footer />
+    </BrowserRouter>
+  );
+
+  test('Natural', () => {
+    expect(footer).toMatchSnapshot();
+  });
+
+  test('Category Select check', () => {
+    footer.find({ id: 'select-category' }).simulate('click');
+    footer
+      .find({ role: 'option', 'data-value': 'Campaigns' })
+      .at(0)
+      .simulate('click');
+    expect(footer.find({ id: 'select-category' }).text()).toBe('Campaigns');
+  });
+
+  test('Location Select check', () => {
+    footer.find({ id: 'select-location' }).simulate('click');
+    footer
+      .find({ role: 'option', 'data-value': 'London' })
+      .at(0)
+      .simulate('click');
+    expect(footer.find({ id: 'select-location' }).text()).toBe('London');
+  });
+
+  test('Year Select check', () => {
+    footer.find({ id: 'select-year' }).simulate('click');
+    footer
+      .find({ role: 'option', 'data-value': 1970 })
+      .at(0)
+      .simulate('click');
+    expect(footer.find({ id: 'select-year' }).text()).toBe('1970');
+  });
 });
