b6c63b395ae5a6131babb22a611573a805c771af
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-23 15:46:21 +0300

    refactor BE to send story status

--- src/controllers/getStory.js
+++ src/controllers/getStory.js
@@ -5,7 +5,9 @@ const getStory = (req, res) => {
   const allowedFields = ['title', 'details', 'month', 'year', 'imageUrl', 'imageCaption', 'tags', 'category', 'status'];
 
   Story.find(`${id}`, (err, record) => {
-    if (err) { res.json({ success: false, message: err.message }); return; }
+    if (err) {
+      res.json({ success: false, data: { message: `${err.error} ${err.statusCode} : ${err.message}.` } }); return;
+    }
     // eslint-disable-next-line no-underscore-dangle
     const rawObj = record._rawJson.fields;
     const allowedObj = allowedFields.reduce((result, ele) => {
@@ -13,7 +15,12 @@ const getStory = (req, res) => {
       result[ele] = rawObj[ele];
       return result;
     }, {});
-    res.json({ success: true, data: allowedObj });
+    allowedObj.body = true;
+    if (allowedObj.status) {
+      res.json({ success: true, data: allowedObj });
+    } else {
+      res.json({ success: false, data: { message: 'The story you requested is still pending approval.' } });
+    }
   });
 };
 
