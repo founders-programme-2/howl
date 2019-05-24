0f2c54ca9bc2d585956499a2c13b5435ccb0717b
Kristina Jaggard <jaggardk@gmail.com>
2019-05-24 13:39:30 +0100

    push for olly

--- src/controllers/filter.js
+++ src/controllers/filter.js
@@ -21,23 +21,29 @@ const filter = (req, res) => {
         direction: 'desc',
       },
     ],
+    filterByFormula: ADD(),
   }).eachPage(
     (records, fetchNextPage) => {
       // This function will get called for each page of records.
 
       records.forEach((record) => {
-        // REQUEST VARIABLES
-        const reqCategory = req.body.category;
-        const reqYear = req.body.year;
-        const reqLocation = req.body.location;
+        filteredData.push(record);
 
-        // RESPONSE VARIABLES
-        const { category, year, location } = record.fields;
-        console.log(location);
+        // // REQUEST VARIABLES
+        // const reqCategory = req.body.category;
+        // const reqYear = req.body.year;
+        // const reqLocation = req.body.location;
+        // const reqTags = req.body.tags;
 
-        if (reqCategory == category && reqYear == year && reqLocation == location) {
-          filteredData.push(record);
-        }
+        // // RESPONSE VARIABLES
+        // const { category, year, location } = record.fields;
+
+        // // THIS FILTER IS FOR REQUIRED FORM PARAMS
+        // if (reqCategory == category && reqYear == year && reqLocation == location) {
+        //   filteredData.push(record);
+        // }
+
+        // // THIS FILTER IS FOR CONDITIONAL FORM PARAMS
       });
       // To fetch the next page of records, call `fetchNextPage`.
       // If there are more records, `page` will get called again.
