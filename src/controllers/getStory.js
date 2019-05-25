b1a262d4632edf53db5aa5f76c61716e7ac46999
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-25 19:16:16 +0300

    refactor errors sent format and assign body=true to data in frontend instead

--- src/controllers/getStory.js
+++ src/controllers/getStory.js
@@ -4,9 +4,9 @@ const getStory = (req, res) => {
   const { id } = req.params;
   const allowedFields = ['title', 'details', 'month', 'year', 'imageUrl', 'imageCaption', 'tags', 'category', 'status'];
 
-  Story.find(`${id}`, (err, record) => {
+  Story.find(id, (err, record) => {
     if (err) {
-      res.json({ success: false, data: { message: `${err.error} ${err.statusCode} : ${err.message}.` } }); return;
+      res.json({ success: false, err: `${err.message}.` }); return;
     }
     // eslint-disable-next-line no-underscore-dangle
     const rawObj = record._rawJson.fields;
@@ -15,11 +15,11 @@ const getStory = (req, res) => {
       result[ele] = rawObj[ele];
       return result;
     }, {});
-    allowedObj.body = true;
+
     if (allowedObj.status) {
       res.json({ success: true, data: allowedObj });
     } else {
-      res.json({ success: false, data: { message: 'The story you requested is still pending approval.' } });
+      res.json({ success: false, err: 'The story you requested is still pending approval.' });
     }
   });
 };
