40f21927beba16d82b874ea116135db79164bb9c
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-26 13:34:29 +0300

    include name and creation date in retrieved data fields

--- src/controllers/getStory.js
+++ src/controllers/getStory.js
@@ -1,21 +1,23 @@
+/* eslint-disable no-underscore-dangle */
 const { Story } = require('../airtables');
 
 const getStory = (req, res) => {
   const { id } = req.params;
-  const allowedFields = ['title', 'details', 'month', 'year', 'imageUrl', 'imageCaption', 'tags', 'category', 'status'];
+  const allowedFields = ['name', 'title', 'details', 'month', 'year', 'imageUrl', 'imageCaption', 'tags', 'category', 'status'];
 
   Story.find(id, (err, record) => {
     if (err) {
       res.json({ success: false, err: `${err.message}.` }); return;
     }
-    // eslint-disable-next-line no-underscore-dangle
     const rawObj = record._rawJson.fields;
+    const createdAt = record._rawJson.createdTime.slice(0, 10);
     const allowedObj = allowedFields.reduce((result, ele) => {
       // eslint-disable-next-line no-param-reassign
       result[ele] = rawObj[ele];
       return result;
     }, {});
 
+    allowedObj.timeCreated = createdAt;
     if (allowedObj.status) {
       res.json({ success: true, data: allowedObj });
     } else {
