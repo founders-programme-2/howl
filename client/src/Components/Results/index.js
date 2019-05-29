8ca0ff05b6e8ba32e58d540061586fc77d675166
mhmdtshref <mhmdtshref@gmail.com>
2019-05-30 02:09:16 +0300

    initial results rendering #246

--- client/src/Components/Results/index.js
+++ client/src/Components/Results/index.js
@@ -1,10 +1,15 @@
 import React, { Fragment } from 'react';
 import Footer from '../Common/Footer';
 
-const Results = ({ footerProp }) => {
+const Results = ({ footerProp, results }) => {
+  const renderResults = res => {
+    return res.map(story => <p>{story}</p>);
+  };
+
   return (
     <Fragment>
       <h1>Hello Results page!</h1>
+      {renderResults(results)}
       <Footer updateResults={footerProp} />
     </Fragment>
   );
