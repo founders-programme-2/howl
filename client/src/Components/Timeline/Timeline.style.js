251bc780d331e22e20c250a4e2c96e9a3cda73c9
Orjwan Al-Rajaby <orjwan98@hotmail.com>
2019-05-31 11:48:39 +0300

    make it responsive

--- client/src/Components/Timeline/Timeline.style.js
+++ client/src/Components/Timeline/Timeline.style.js
@@ -1,8 +1,15 @@
 import styled from 'styled-components';
 
+import responsiveSizes from '../../constants/responsiveSizes';
+
+const { SML } = responsiveSizes;
+
 const Picture = styled.img`
   width: 70%;
   margin: 2rem auto;
+  @media (max-width: ${SML}) {
+    width: 100%;
+  }
 `;
 
 const ImgFigure = styled.figure`
@@ -16,6 +23,9 @@ const ImgFigure = styled.figure`
 const FigCap = styled.figcaption`
   width: 80%;
   margin: 0 auto;
+  @media (max-width: ${SML}) {
+    width: 100%;
+  }
 `;
 
 export { Picture, ImgFigure, FigCap };
